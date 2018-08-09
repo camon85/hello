d20689c83cbbd7a4ff5c8a10679208ff2c7f2f8e
camon <camon85@gmail.com>
2018-08-10 02:05:03 +0900

    init

---
+++ src/test/java/io/github/camon85/hello/HelloApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.camon85.hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
